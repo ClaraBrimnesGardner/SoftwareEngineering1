51a6138d4fdf98c28b800557db356927a4121e01
Clara Brimnes Gardner <clarabrimnesgardner@claras-air.eduroam.wireless.dtu.dk>
2017-03-27 13:49:48 +0200

    HEJ

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+/**
+ * Created by clarabrimnesgardner on 27/03/2017.
+ */
+public class Employee {
+    String ID;
+    int RegistreredTime;
+
+    // Constructor
+    public Employee(String ID){
+        this.ID=ID;
+    }
+
+
+}
