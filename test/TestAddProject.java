ff09a78854d60623aea12832490d0b795add61f1
SaraDortheaNielsen <sara.d.nielsen@hotmail.com>
2017-04-04 14:12:08 +0200

    project

---
+++ test/TestAddProject.java
@@ -0,0 +1,19 @@
+/**
+ * Created by sarad on 04-04-2017.
+ */
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAddProject {
+    /*
+    Tests that the method addProject adds one Project with the correct ID
+     */
+    @Test
+    public void testAddProject(){
+        System SoftwareHouse = new System();
+        int ID = 1234;
+        //Employee.addProject(ID);
+
+    }
+}
+
