80b8848132958f5110357d7f932ea71f13e088cc
ClaraBrimnesGardner <s153542@student.dtu.dk>
2017-04-11 15:27:52 +0200

    Hubbit

--- test/TestAddProject.java
+++ test/TestAddProject.java
@@ -1,9 +1,6 @@
-/**
- * Created by sarad on 04-04-2017.
- */
-import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class TestAddProject {
     /*
     Tests that the method addProject adds one Project with the correct ID
