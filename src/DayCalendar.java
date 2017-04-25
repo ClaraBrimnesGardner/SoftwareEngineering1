f8e028cf76e798bcb75f84cc1da83fb1a62a7bac
ClaraBrimnesGardner <s153542@student.dtu.dk>
2017-04-25 14:24:55 +0200

    Hubbit

---
+++ src/DayCalendar.java
@@ -0,0 +1,17 @@
+/**
+ * Created by clarabrimnesgardner on 25/04/2017.
+ */
+public class DayCalendar {
+    WeekCalendar weekCalendar;
+    int dayNumber;
+
+    // Constructor
+    public DayCalendar(WeekCalendar weekCalendar, int dayNumber){
+        this.weekCalendar=weekCalendar;
+        this.dayNumber=dayNumber;
+    }
+
+
+
+
+}
---
+++ src/DayRegistration.java
@@ -0,0 +1,22 @@
+/**
+ * Created by clarabrimnesgardner on 25/04/2017.
+ */
+public class DayRegistration {
+    private DayCalendar dayCalendar;
+    private int registeredHours;
+
+    // Constructor
+    public DayRegistration(DayCalendar dayCalendar, int registeredHours){
+        this.dayCalendar=dayCalendar;
+        this.registeredHours=registeredHours;
+    }
+
+    // Getter methods
+    public DayCalendar getDayCalendar(){
+        return dayCalendar;
+    }
+
+    public int getRegisteredHours(){
+        return  registeredHours;
+    }
+}
